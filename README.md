d56aa552d355530d097da9f63d1db5f0cec60908
spr15bd <bevandady@gmail.com>
2019-07-22 22:51:33 +0100

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# space-vulture-blast
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./styles.css";
+
+let canvas = document.getElementById("gameScreen");
+let ctx = canvas.getContext("2d");
+
+ctx.fillRect(40, 50, 30, 30);
