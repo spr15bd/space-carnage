10f2bc197f750c533a3b6f94671e6b68a2bddd7d
spr15bd <bevandady@gmail.com>
2019-07-26 00:20:46 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import "./styles.css";
-
+import Player from "./player.js";
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
 
-const SCREEN_WIDTH = 1280;
-const SCREEN_HEIGHT = 1024;
+const SCREEN_WIDTH = 800;
+const SCREEN_HEIGHT = 600;
 
 ctx.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+let player = new Player(SCREEN_WIDTH, SCREEN_HEIGHT);
+player.draw(ctx);
--- src/player.js
+++ src/player.js
@@ -1,7 +1,16 @@
-class Player {
+export default class Player {
   constructor(screenWidth, screenHeight) {
     // declare player properties
+    this.width = 150;
+    this.height = 30;
+    this.position = {
+      x: screenWidth / 2 - this.width / 2,
+      y: screenHeight - this.height - 10
+    };
   }
 
-  draw(ctx) {}
+  draw(ctx) {
+    ctx.fillStyle = "red";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
 }
