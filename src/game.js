c16b9b28716438963f7470aac3b13b697b4f5d1a
spr15bd <bevandady@gmail.com>
2019-11-21 23:57:58 +0000

    Add files via upload

--- src/game.js
+++ src/game.js
@@ -423,40 +423,36 @@ export default class Game {
       } else if (enemy.enemyType === 3) {
         if (enemy.movement === 0) {
           enemy.position.x =
-            410 * Math.sin(Date.now() * 0.00145) + enemy.start.x;
+            414 * Math.sin(Date.now() * 0.0015) + enemy.start.x;
           if (Date.now() - this.level.startEnemyWaveCycle >= 10000) {
             enemy.movement += 1;
           }
         } else if (enemy.movement === 1) {
-          enemy.position.x += delta / 1000;
-          enemy.position.y += delta / 1000;
-          if (Math.abs(enemy.position.x - enemy.start.x) > 24) {
-            enemy.position.x -= 25;
-            enemy.position.y -= 25;
-            enemy.movement += 1;
+          //enemy.position.x += 0.4;
+          enemy.position.y += 2;
+          if (Math.abs(enemy.position.y - enemy.start.y) > 200) {
+            //enemy.position.x -= 200;
+            //enemy.position.y -= 200;
+            enemy.movement = 2;
           }
         } else if (enemy.movement === 2) {
-          enemy.position.x -= delta / 1000;
-          enemy.position.y -= delta / 1000;
-          if (Math.abs(enemy.position.x - enemy.start.x) > 24) {
-            enemy.position.x -= 25;
-            enemy.position.y -= 25;
+          enemy.angle = 0;
+          enemy.position.x -= 4;
+          //enemy.position.y -= 0.4;
+          if (enemy.position.x < 0) {
             enemy.movement += 1;
           }
         } else if (enemy.movement === 3) {
-          enemy.position.x += delta / 1000;
-          enemy.position.y -= delta / 1000;
-          if (Math.abs(enemy.position.x - enemy.start.x) > 24) {
-            enemy.position.x -= 25;
-            enemy.position.y += 25;
+          enemy.angle = 180;
+          enemy.position.x += 4;
+          //enemy.position.y -= 1;
+          if (enemy.position.x > this.screenWidth) {
             enemy.movement += 1;
           }
         } else if (enemy.movement === 4) {
-          enemy.position.x -= delta / 1000;
-          enemy.position.y += delta / 1000;
-          if (Math.abs(enemy.position.x - enemy.start.x) > 24) {
-            enemy.position.x += 25;
-            enemy.position.y -= 25;
+          enemy.angle = 90;
+          enemy.moveTo(enemy.start.x, enemy.start.y);
+          if (Math.abs(enemy.position.x - enemy.start.x) < 24) {
             enemy.movement = 0;
           }
         }
